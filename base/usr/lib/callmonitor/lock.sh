f0189ec840c6aa11b252d962ace758f36e58ab92
Andreas Bühmann <andreas.buehmann@web.de>
2005-10-28 12:03:54 +0000

    git-svn-id: https://callmonitor.svn.sourceforge.net/svnroot/callmonitor/callmonitor@43 e872857c-5b07-0410-aa81-e3586f851bf3

--- base/usr/lib/callmonitor/lock.sh
+++ base/usr/lib/callmonitor/lock.sh
@@ -1,20 +1,18 @@
-# lock $file by creating a directory $file.lock
+# lock $file by creating a symlink $file.lock -> PID
 lock() {
     local file="$(realpath "$1")" interval="${2:-1000000}" first=1
     local lock="$file.lock"
-    while ! mkdir "$lock" 2> /dev/null; do
+    while ! ln -s $$ "$lock" 2> /dev/null; do
 	if [ $first -eq 1 ]; then 
 	    first=0
 	    echo "Waiting for exclusive lock on $file" 1>&2
 	fi
 	usleep $interval
     done
-    # echo $$ > "$lock/owner"
 }
 
 unlock() {
     local file="$(realpath "$1")"
     local lock="$file.lock"
-    # rm "$lock/owner"
-    rmdir "$lock"
+    rm "$lock"
 }

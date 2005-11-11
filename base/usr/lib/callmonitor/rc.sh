c144b28e927c7649b56b14e6874aa3354d062a2a
Andreas Bühmann <andreas.buehmann@web.de>
2005-11-11 17:41:29 +0000

    git-svn-id: https://callmonitor.svn.sourceforge.net/svnroot/callmonitor/fritzbox/callmonitor/trunk@87 e872857c-5b07-0410-aa81-e3586f851bf3

--- base/usr/lib/callmonitor/rc.sh
+++ base/usr/lib/callmonitor/rc.sh
@@ -1,11 +1,3 @@
-DAEMON=callmonitor
-TELEFON="$CALLMONITOR_LIBDIR/telefon"
-FIFO="/var/run/$DAEMON/fifo"
-
-if [ ! -d /var/run/$DAEMON ]; then
-	mkdir -p /var/run/$DAEMON
-fi
-
 check_status() {
 	local exitval="${1:-$?}"
 	if [ "$exitval" -eq 0 ]; then
@@ -14,30 +6,4 @@ check_status() {
 		echo "failed."
 	fi
 	return $exitval
-}
-
-start_telefon() {
-	echo -n "Starting telefon..."
-	"$TELEFON" < /dev/null 1>&3 3>&- 2> /dev/null
-	check_status
-}
-stop_telefon() {
-	echo -n "Stopping telefon..."
-	killall telefon > /dev/null 2>&1
-	check_status
-}
-
-start_daemon() {
-	if [ ! -e /mod/etc/callmonitor.listeners ]; then
-		ln -sf "$CALLMONITOR_LISTENERS" /mod/etc/callmonitor.listeners
-	fi
-
-	echo -n "Starting callmonitor..."
-	"$DAEMON" > /dev/null 2>&1
-	check_status
-}
-stop_daemon() {
-	echo -n "Stopping callmonitor..."
-	"$DAEMON" -s
-	check_status
 }

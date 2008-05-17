fd44e1dea9a723b8365660a0c8dac35072cb3403
Andreas Bühmann <andreas.buehmann@web.de>
2008-05-17 11:32:14 +0000

    html() is globally available in Freetz
    git-svn-id: https://callmonitor.svn.sourceforge.net/svnroot/callmonitor/fritzbox/callmonitor/trunk@464 e872857c-5b07-0410-aa81-e3586f851bf3

--- mod/dsmod/root/usr/lib/callmonitor/modules/cgi.sh
+++ mod/dsmod/root/usr/lib/callmonitor/modules/cgi.sh
@@ -21,23 +21,6 @@
 ##
 . /usr/lib/libmodcgi.sh
 
-html() {
-    if ? "$# == 0"; then
-	sed -e '
-	    s/&/\&amp;/g
-	    s/</\&lt;/g
-	    s/>/\&gt;/g
-	    s/'\''/\&#39;/g
-	    s/"/\&quot;/g
-	'
-    else
-	case $* in
-	    *[\&\<\>\'\"]*) httpd -e "$*" ;;
-	    *) echo "$*" ;;
-	esac
-    fi
-}
-
 pre() {
     echo -n "<pre>"
     html # stdin

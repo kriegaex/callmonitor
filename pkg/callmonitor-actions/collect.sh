e3228b5f7bf2b2635e9a7f3e85b7f444a10c6e17
Andreas Bühmann <andreas.buehmann@web.de>
2009-05-30 19:03:59 +0000

    Revert changes; this reorganization requires better planning
    git-svn-id: https://callmonitor.svn.sourceforge.net/svnroot/callmonitor/fritzbox/callmonitor/trunk@507 e872857c-5b07-0410-aa81-e3586f851bf3

--- pkg/callmonitor-actions/collect.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-BNAME=$1
-TAR=tar
-$TAR c --exclude=.svn -C actions . |  $TAR x -C "$BNAME"/root
-$TAR c --exclude=.svn -C freetz-base . |  $TAR x -C "$BNAME"/root
-$TAR c --exclude=.svn -C freetz-actions . |  $TAR x -C "$BNAME"/root
-$TAR c --exclude=.svn -C actions-config . |  $TAR x -C "$BNAME"/root
-cp freetz/.language "$BNAME"
--- pkg/callmonitor/collect.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-BNAME=$1
-TAR=tar
-$TAR c --exclude=.svn -C callmonitor . |  $TAR x -C "$BNAME"/root
-$TAR c --exclude=.svn -C actions . |  $TAR x -C "$BNAME"/root
-$TAR c --exclude=.svn -C freetz-base . |  $TAR x -C "$BNAME"/root
-$TAR c --exclude=.svn -C freetz-actions . |  $TAR x -C "$BNAME"/root
-$TAR c --exclude=.svn -C freetz . |  $TAR x -C "$BNAME"

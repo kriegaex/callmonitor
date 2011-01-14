0a1c79889d581db162f59bee8a2832e05176eaf6
Andreas Bühmann <andreas.buehmann@web.de>
2011-01-14 18:44:36 +0000

    Reverse lookup for Italian numbers (via pronto.it)
    git-svn-id: https://callmonitor.svn.sourceforge.net/svnroot/callmonitor/fritzbox/callmonitor/trunk@588 e872857c-5b07-0410-aa81-e3586f851bf3

---
+++ base/usr/lib/callmonitor/reverse/pronto_it.sh
@@ -0,0 +1,31 @@
+_reverse_pronto_it_url() {
+    local number="0${1#${LKZ_PREFIX}39}"
+    URL="http://pronto.it/?q=$(urlencode "$number")"
+}
+_reverse_pronto_it_request() {
+    local URL=
+    _reverse_pronto_it_url "$@"
+    wget_callmonitor "$URL" -q -O -
+}
+
+_reverse_pronto_it_extract() {
+    sed -n -e '
+    	/Non ho trovato/ {
+	    '"$REVERSE_NA"'
+	}
+	\#<div class="blocco_info"#,\#</div># {
+	    \#<span class="cognome# b name
+	    \#<span class="via"# H
+	    \#</div># b output
+	}
+	b
+	: name
+	s#.*#<rev:name>&</rev:name>#
+	h
+	b
+	: output
+	g
+	'"$REVERSE_SANITIZE"'
+	'"$REVERSE_OK"'
+    '
+}

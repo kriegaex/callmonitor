e1f111bc4f62fb241bcc3be4e80cdcf2f8de49cd
Andreas Bühmann <andreas.buehmann@web.de>
2013-02-10 11:17:34 +0000

    Release 1.20.7
    git-svn-id: https://callmonitor.svn.sourceforge.net/svnroot/callmonitor/trunk@677 e872857c-5b07-0410-aa81-e3586f851bf3

--- base/usr/lib/callmonitor/web/config/60-credentials.sh
+++ base/usr/lib/callmonitor/web/config/60-credentials.sh
@@ -3,20 +3,31 @@ require user
 for once in my_life; do ## ;-) to allow "break"ing
 
 user_supported || break
-# user_is_compat && break
-# user_auth_is_skipped && break
+compat=false
+user_is_compat && compat=true
+skipped=false
+user_auth_is_skipped && skipped=true
 
 sec_begin '$(lang de:"Zugangsdaten" en:"Credentials")'
 
+echo "<p>$(lang
+    de:"Der Callmonitor greift für bestimmte Funktionalitäten auf die AVM-Weboberfläche zu und muss sich dort einloggen können."
+    en:"The Callmonitor accesses the AVM web interface for certain functionalities and must be able to log in."
+)</p>"
+
+echo "<p>$(lang de:"Aktuell gewählter Modus" en:"Currently selected mode"): "
+if $skipped; then
+    echo "$(lang de:"Kein Passwort" en:"No password")"
+elif $compat; then
+    echo "$(lang de:"Einzelbenutzer" en:"Single-user")"
+else
+    echo "$(lang de:"Mehrbenutzer" en:"Multi-user")"
+fi
+echo "</p>"
+
 echo "
 <p>
-$(lang 
-    de:"Für den Zugriff auf die AVM-Weboberfläche im Mehrbenutzermodus (Anmeldung mit FRITZ!Box-Benutzernamen und Kennwort)"
-    en:"For accessing the AVM web interface in multi-user mode"
-)
-</p>
-<p>
-<label for='username'>$(lang de:"Benutzername" en:"Username"):</label>
+<label for='username'>$(lang de:"Benutzername im Mehrbenutzer-Modus" en:"Username in multi-user mode"):</label>
 <select name='username' id='username'>
 "
 { user_list; echo "$CALLMONITOR_USERNAME"; } | sort -u | 

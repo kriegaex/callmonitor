0ab721bef19e7a8ec61ee0ed9ccd3eab219bcf4f
Alexander Kriegisch <Alexander@Kriegisch.name>
2014-05-23 00:45:23 +0200

    Fix reverse number search for herold.at

--- base/usr/lib/callmonitor/reverse/herold_at.sh
+++ base/usr/lib/callmonitor/reverse/herold_at.sh
@@ -2,29 +2,31 @@ _reverse_herold_at_url() {
     local number="0${1#+43}"
     URL="http://www.herold.mobi/redirect/findlisting?what=$(urlencode "$number")&searchtype=WHITEPAGES"
 }
+
 _reverse_herold_at_request() {
     local URL=
     _reverse_herold_at_url "$@"
     wget_callmonitor -q -O - "$URL"
 }
+
 _reverse_herold_at_extract() {
     sed -n -e '
-	/keine Treffer/ {
-	    '"$REVERSE_NA"'
-	}
-	## very fragile
-	/^[[:space:]]*<div class="result_parent"/,\#<br/>[[:space:]]*$# {
-	    \#<br/>[[:space:]]*$# b cleanup
-	    H
-	}
-	b
-	
-	: cleanup
-	g
-	s#.*<b>\([^<]*\)</b>#<rev:name>\1</rev:name>#
-	s#<br/>#, #g
-	'"$REVERSE_DECODE_ENTITIES_UTF8"'
-	'"$REVERSE_SANITIZE"'
-	'"$REVERSE_OK"'
+    /keine Treffer/ {
+        '"$REVERSE_NA"'
+    }
+    /myMap\.addMarker/ {
+        s/.*new Map([^)]\+);myMap\.addMarker/\n###/
+        s/myMap\.addMarker.*//
+        s/.*###.*.title.:.\(.*\).,.tel.:.*.,.addr.:.\(.*\).,.icon.*/<rev:name>\1<\/rev:name>\2/
+        h
+    }
+    $ {
+        g
+        s#<br/>#, #g
+        '"$REVERSE_DECODE_ENTITIES_UTF8"'
+        '"$REVERSE_SANITIZE"'
+        '"$REVERSE_OK"'
+        p
+    }
     ' | utf8_latin1
 }

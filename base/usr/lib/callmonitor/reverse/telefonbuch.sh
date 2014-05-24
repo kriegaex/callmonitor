471f23c761dee88e23168dcbeb66662421ee106a
Andreas Bühmann <andreas.buehmann@web.de>
2014-05-24 09:57:29 +0200

    telefonbuch: Simplify, quit after first hit
    Additionally, replace non-breaking spaces (\xa0 in Latin-1)

--- base/usr/lib/callmonitor/reverse/telefonbuch.sh
+++ base/usr/lib/callmonitor/reverse/telefonbuch.sh
@@ -8,39 +8,33 @@ _reverse_telefonbuch_request() {
     _reverse_telefonbuch_url "$@"
     wget_callmonitor -q -O - "$URL" 2>&1
 }
+REVERSE_TELEFONBUCH_NBSP=$(echo -e '\xa0')
 _reverse_telefonbuch_extract() {
     sed -n -e '
         /wget: server returned error: HTTP.* 410 Gone\|Kein Treffer gefunden/ {
             '"$REVERSE_NA"'
         }
         /<a id="name0"/,/<\/address>/ {
-            s/^[ \t]*//
             /<a id="name0"/,/<\/a>/ {
+                H
                 /<\/a>/ {
-                    s/^[ \t]*\(.*\)<\/a>/<rev:name>\1<\/rev:name>/
+                    g
+                    s/.*/<rev:name>&<\/rev:name>/
                     h
                 }
             }
             /<address class=/,/<\/address>/ {
-                /<\/address>/ {
-                    s/<[^>]*>/#/g
-                    s/&nbsp;/ /g
-                    s/,/#/g
-                    s/\([ \t]*#[ \t]*\)\+/#/g
-                    s/#$//
-                    H
-                }
+                H
+                /<\/address>/ b found
             }
         }
-        $ {
-            g
-            s/\n//g
-            s/#\+/\; /
-            s/#\+/, /g
-            '"$REVERSE_DECODE_ENTITIES"'
-            '"$REVERSE_SANITIZE"'
-            '"$REVERSE_OK"'
-            p
+        b
+        :found
+        g
+        s/'$REVERSE_TELEFONBUCH_NBSP'/ /g
+        '"$REVERSE_DECODE_ENTITIES"'
+        '"$REVERSE_SANITIZE"'
+        '"$REVERSE_OK"'
         }
    '
 }

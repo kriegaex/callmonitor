f898dedb6bd8673bb9498136c2d83a6fc1f95a9b
ste-fan <s.t.e-f.a.n@web.de>
2014-10-12 21:51:54 +0200

    Add mail action mailmessage_html
    * also modify functions in message module and mail action to
      support creation of HTML code
    
    * mailmessage_html:
      + sends mail as HTML + plain text
      + remote number is displayed as a link to either e.g.
        - tel:+493012345678 (clickable on smartphones) or
        - sip:username@siphost.net

--- base/usr/lib/callmonitor/modules/message.sh
+++ base/usr/lib/callmonitor/modules/message.sh
@@ -1,13 +1,15 @@
 require format
+require tel
 
 ## default message (multi-line)
 default_message() {
-    local cols=${1:-10000} lines=${2:-10000}
+    local cols=${1:-10000} lines=${2:-10000} type=$3  # '':text, 1:html
     
     local call="$(lang de:"Anruf" en:"Call")"
     local from="$(lang de:"von" en:"from")" to="$(lang de:"an" en:"to")"
 
     local here here_entry here_dir there there_entry there_dir
+    local p1 p2 br a1 a2 __
     case $EVENT in
 	in:*)
 	    there_dir=$from there=$SOURCE_DISP there_entry=$SOURCE_ENTRY
@@ -19,21 +21,40 @@ default_message() {
 	    ;;
     esac
     
+    if ? 'type == 1'; then
+	p1='<p>'
+	p2='</p>\n'
+	br='<br>'
+	if [ -n "$there" ]; then
+	    normalize_address "$there"
+	    case "$__" in
+		*@*) __="sip:$__" ;;
+		*) __="tel:$__" ;;
+	    esac
+	    a1="<a href=$__>"
+	    a2='</a>'
+	fi
+    fi
+
+    echo -n "$p1"
     if ! empty "$here" && ? "lines > 1"; then
-	echo "$call $here_dir ${here_entry:-$here}"
+	echo "$call $here_dir ${here_entry:-$here}$br"
     elif ? "lines == 1"; then
 	echo -n "$call "
     else
-	echo "$call"
+	echo "$call$br"
     fi
     if ! empty "$there"; then
 	if ? "lines >= 3"; then
-	    echo "$there_dir $there"
-	    wrap "$cols" "$there_entry"
+	    echo "$there_dir $a1$there$a2$br"
+	    wrap "$cols" "$there_entry" $type
 	else
-	    echo "$there_dir ${there_entry:-$there}" | cut -c "1-$cols"
+	    echo "$there_dir $a1${there_entry:-$there}" |
+		cut -c "1-$(($cols+${#a1}))" | tr -d '\n'
+	    echo "$a2"
 	fi
     fi
+    echo -ne "$p2"
 }
 
 ## one-liner
@@ -47,7 +68,7 @@ default_raw() {
 
 ## wrap text (line length <= max)
 wrap() {
-    local max=${1:-10000} text=$2
+    local max=${1:-10000} text=$2 type=$3
     local len=${#text}
     if ? "len == 0"; then
 	return
@@ -58,6 +79,7 @@ wrap() {
 	while ? "a <= len"; do
 	    expr substr "$text" $a $max
 	    let a+=max
+	    ? 'type == 1' && ? "a <= len" && echo '<br>'
 	done
     fi
 }
